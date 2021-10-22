daa5183421ad910516fc323a933c39be7892bd1f
Volodymyr Komarov <vovikems@gmail.com>
2021-10-22 19:45:45 +0300

    Add test for MySQL driver to handle "Packets out of order" error
    Added integration test for MySQL driver to validate
    that "Packets out of order" error are correctly mapped
    to ConnectionException  instead of StatementException
    and trigger reconnection.

--- tests/Database/Driver/MySQL/ExceptionsTest.php
+++ tests/Database/Driver/MySQL/ExceptionsTest.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace Spiral\Database\Tests\Driver\MySQL;
 
+use Spiral\Database\Exception\StatementException\ConnectionException;
+
 /**
  * @group driver
  * @group driver-mysql
@@ -18,4 +20,29 @@ namespace Spiral\Database\Tests\Driver\MySQL;
 class ExceptionsTest extends \Spiral\Database\Tests\ExceptionsTest
 {
     public const DRIVER = 'mysql';
+
+    protected function getConnectionId(): int
+    {
+        return (int) $this->database->query("SELECT CONNECTION_ID() AS id;")->fetchAll()[0]['id'] ?? 0;
+    }
+
+    public function testPacketsOutOfOrderConsideredAsConnectionException(): void
+    {
+        $connectionId = $this->getConnectionId();
+
+        // Prepare connection to generate "Packets out of order. Expected 1 received 0. Packet size=145"
+        // at the next query response
+        $this->database->query("SET SESSION wait_timeout=1")->fetch();
+        sleep(1);
+
+        try {
+            $newConnectionId = $this->getConnectionId();
+            $this->assertNotEquals(0, $newConnectionId);
+        } catch (\RuntimeException $e) {
+            $this->assertInstanceOf(ConnectionException::class, $e);
+            return;
+        }
+
+        $this->assertNotSame($connectionId, $newConnectionId, 'Expected reconnect for database connection');
+    }
 }
