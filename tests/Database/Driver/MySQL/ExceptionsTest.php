632488c5f7dfd3d4dc47edc9f1c2c992724e7526
Volodymyr Komarov <vovikems@gmail.com>
2021-11-03 19:51:47 +0200

    Map `mysqlnd` `Packets out of order` error as ConnectionException (#83)

--- tests/Database/Driver/MySQL/ExceptionsTest.php
+++ tests/Database/Driver/MySQL/ExceptionsTest.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
 
 namespace Spiral\Database\Tests\Driver\MySQL;
 
+use Spiral\Database\Exception\StatementException\ConnectionException;
+
 /**
  * @group driver
  * @group driver-mysql
@@ -18,4 +20,24 @@ namespace Spiral\Database\Tests\Driver\MySQL;
 class ExceptionsTest extends \Spiral\Database\Tests\ExceptionsTest
 {
     public const DRIVER = 'mysql';
+
+    public function testPacketsOutOfOrderConsideredAsConnectionExceptionFromPHP74(): void
+    {
+        if (PHP_VERSION_ID < 70400) {
+            $this->markTestSkipped('Expecting PHP version >=7.4. Skipped due to ' . PHP_VERSION);
+        }
+
+        // Prepare connection to generate "Packets out of order. Expected 1 received 0. Packet size=145"
+        // at the next query response
+        $this->database->query("SET SESSION wait_timeout=1")->fetch();
+        sleep(1);
+
+        try {
+            $result = $this->database->query('SELECT version() AS version')->fetchAll();
+            $this->assertNotEmpty($result[0]['version'] ?? '', 'Expected result from second query');
+        } catch (\RuntimeException $e) {
+            $this->assertInstanceOf(ConnectionException::class, $e);
+            return;
+        }
+    }
 }
